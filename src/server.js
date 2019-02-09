9701fba05a7fe126db97c801afd9962c3acc4922
Jedediah Arnold <jarnold910@gmail.com>
2019-02-09 12:59:53 -0500

    production environment and middleware configured

--- src/server.js
+++ src/server.js
@@ -1,9 +1,26 @@
 const app = require("./app");
 const http = require("http");
+
+const port = normalizePort(process.env.PORT || "5000");
+app.set("port", port);
+
 const server = http.createServer(app);
 
-server.listen(5000);
+server.listen(port);
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
 
 server.on("listening", () => {
-  console.log("server is listening for requests on port 5000");
+  console.log(
+    `server is listening for requests on port ${server.address().port}`
+  );
 });

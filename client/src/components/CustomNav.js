869c4a07bacf347725841ed5380bf76a2e2194b1
Jedediah Arnold <jarnold910@gmail.com>
2019-02-13 13:34:55 -0500

    beginning to assemble my list and functions

--- client/src/components/CustomNav.js
+++ client/src/components/CustomNav.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Redirect, Link } from "react-router-dom";
+import axios from "axios";
 
 class CustomNav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toLanding: false,
+      email: localStorage.getItem("user")
+    };
+    this.handleSignOut = this.handleSignOut.bind(this);
+  }
+  handleSignOut() {
+    console.log("made it to sign out");
+    axios.get("/users/sign_out").then(res => {
+      localStorage.removeItem("user", this.email);
+      localStorage.removeItem("userId", this.id);
+      this.setState({ toLanding: true });
+    });
+  }
   render() {
+    if (this.state.toLanding) {
+      return <Redirect to="/" />;
+    }
+    const user = localStorage.getItem("user");
     return (
       <section>
         <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
@@ -11,35 +32,41 @@ class CustomNav extends Component {
               FamilyShop
             </Link>
             <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/signin">
-                  Sign In
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="signup">
-                  Sign Up
-                </Link>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Sign Out
-                  <span className="sr-only">(current)</span>
-                </a>
-              </li>
+              {!user && (
+                <li className="nav-item">
+                  <Link className="nav-link" to="/signin">
+                    Sign In
+                  </Link>
+                </li>
+              )}
+              {!user && (
+                <li className="nav-item">
+                  <Link className="nav-link" to="signup">
+                    Sign Up
+                  </Link>
+                </li>
+              )}
+              {user && (
+                <li className="nav-item">
+                  <Link className="nav-link" onClick={this.handleSignOut}>
+                    Sign Out
+                  </Link>
+                </li>
+              )}
             </ul>
           </div>
         </nav>
 
-        <nav class="navbar fixed-bottom navbar-light bg-light">
+        <nav className="navbar fixed-bottom navbar-light bg-light">
           <p>All rights reserved @ FamilyShop 2019</p>
           <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Sign Out
-                <span className="sr-only">(current)</span>
-              </a>
-            </li>
+            {user && (
+              <li className="nav-item">
+                <Link className="nav-link" onClick={this.handleSignOut}>
+                  Sign Out
+                </Link>
+              </li>
+            )}
           </ul>
         </nav>
       </section>
--- client/src/components/List.js
+++ client/src/components/List.js
@@ -5,35 +5,66 @@ class List extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: []
+      email: localStorage.getItem("user"),
+      items: [],
+      title: ""
     };
   }
-  componentWillMount() {
-    axios.get(`/items`).then(res =>
-      this.setState({
-        items: res.data.items
-      })
-    );
+
+  componentWillMount(items) {
+    axios
+      .get("/items", items)
+      .then(res => this.setState({ items: res.data.items }))
+      .catch(err => {
+        console.log(err);
+      });
   }
+
+  addItem(item) {
+    console.log("Made it to item creation", item);
+    axios
+      .post("/item/create", item)
+      .then(res => {})
+      .catch(err => {
+        console.log("At error item create", err);
+      });
+  }
+
+  handleSubmit = e => {
+    const { title } = this.state;
+    e.preventDefault();
+    const item = { title };
+    this.addItem(item);
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
 
   render() {
     console.log(this.state.items);
     return (
       <section className="entire-list-page">
-        <div className="input-group mb-3">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Add item here"
-            aria-label="New Item"
-            aria-describedby="basic-addon2"
-          />
-          <div className="input-group-append">
-            <button className="btn btn-outline-secondary" type="button">
-              Add Item
-            </button>
+        <form className="login-form" onSubmit={this.handleSubmit}>
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="title"
+              value={this.state.content}
+              placeholder="Add item here"
+              aria-label="New Item"
+              aria-describedby="basic-addon2"
+            />
+            <div className="input-group-append">
+              <button className="btn btn-outline-secondary" type="button">
+                Add Item
+              </button>
+            </div>
           </div>
-        </div>
+        </form>
 
         <p>{this.items}</p>
       </section>
